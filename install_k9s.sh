7c29d00c4d39f7522f99a8795eee98d282252967
Joseph Richardson <49208786+JoeHCQ1@users.noreply.github.com>
2022-07-12 16:00:23 -0400

    Pull x64 not arm binary

--- install_k9s.sh
+++ install_k9s.sh
@@ -4,7 +4,7 @@ set -e
 
 pushd /tmp/
 
-curl -LO https://github.com/derailed/k9s/releases/download/v0.25.18/k9s_Linux_arm64.tar.gz
-tar -xzf k9s_Linux_arm64.tar.gz
+curl -LO https://github.com/derailed/k9s/releases/download/v0.25.21/k9s_Linux_x86_64.tar.gz
+tar -xzf k9s_Linux_x86_64.tar.gz
 sudo mv k9s /usr/local/bin/k9s
 
